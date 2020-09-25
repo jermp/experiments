b55c94d23dbd43157b69a74a718bb484e6efe044
Giulio Ermanno Pibiri <jeis90@gmail.com>
2020-09-25 17:42:38 +0200

    faster bytes reverse

--- integer_search_for_strings/test.cpp
+++ integer_search_for_strings/test.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <chrono>
 #include <iomanip>
+#include <immintrin.h>  // for __builtin_bswap64
 
 // g++ -std=c++17 -O3 -march=native integer_search_for_strings.cpp -o
 // integer_search_for_strings
@@ -51,6 +52,7 @@ std::vector<std::string> read_string_collection(char const* filename) {
     return strings;
 }
 
+// generic function
 uint64_t string_to_uint64(std::string const& s) {
     std::string tmp(s.substr(0, prefix_size));
     if (s.size() < prefix_size) {
@@ -58,6 +60,21 @@ uint64_t string_to_uint64(std::string const& s) {
     }
     std::reverse(tmp.begin(), tmp.end());
     return *reinterpret_cast<uint64_t const*>(tmp.data());
+}
+
+// version that assumes a prefix of size 8
+uint64_t string8_to_uint64(std::string const& s) {
+    if (s.size() >= 8) {
+        // option 1. builtin reverse of bytes
+        return __builtin_bswap64(*reinterpret_cast<uint64_t const*>(s.data()));
+
+        // option 2. manual reverse of bytes
+        // uint64_t x = (uint64_t(s[0]) << 56) + (uint64_t(s[1]) << 48) + (uint64_t(s[2]) << 40) +
+        //              (uint64_t(s[3]) << 32) + (uint64_t(s[4]) << 24) + (uint64_t(s[5]) << 16) +
+        //              (uint64_t(s[6]) << 8) + uint64_t(s[7]);
+        // return x;
+    }
+    return string_to_uint64(s);
 }
 
 int main(int argc, char const** argv) {
@@ -99,7 +116,7 @@ int main(int argc, char const** argv) {
     std::vector<uint64_t> uint64_vec;
     uint64_vec.reserve(strings.size());
     for (auto const& s : strings) {
-        uint64_t x = string_to_uint64(s);
+        uint64_t x = string8_to_uint64(s);
         uint64_vec.push_back(x);
         // std::cout << x << std::endl;
         // std::cout << std::bitset<64>(x) << std::endl;
@@ -113,7 +130,7 @@ int main(int argc, char const** argv) {
         uint64_t sum = 0;
         auto start = std::chrono::high_resolution_clock::now();
         for (auto q : queries) {
-            uint64_t x = string_to_uint64(strings[q]);
+            uint64_t x = string8_to_uint64(strings[q]);
             auto it = std::lower_bound(uint64_vec.begin(), uint64_vec.end(), x);
             sum += *it;
         }
